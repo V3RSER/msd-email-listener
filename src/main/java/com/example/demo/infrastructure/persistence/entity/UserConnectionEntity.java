a3078cedea09d434c0d7f1feb8640a2b84e633e5
wizdeiko <wizdeiko@gmail.com>
2026-04-02 05:37:42 +0000

    Fix DataIntegrityViolationException in UserConnection

--- src/main/java/com/example/demo/infrastructure/persistence/entity/UserConnectionEntity.java
+++ src/main/java/com/example/demo/infrastructure/persistence/entity/UserConnectionEntity.java
@@ -3,6 +3,7 @@ package com.example.demo.infrastructure.persistence.entity;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
@@ -10,6 +11,7 @@ import org.springframework.data.relational.core.mapping.Table;
 @Table("user_connections")
 public class UserConnectionEntity {
 
+    @Id
     private UUID id;
 
     @Column("user_id")
